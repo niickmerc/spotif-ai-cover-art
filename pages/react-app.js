44a8579230af4ad2ce1ceaae6f2962ace70dc99a
Anmol Sangha <anmolsangha0@gmail.com>
2023-07-26 16:20:25 -0700

    added first outline for react logic

---
+++ pages/react-app.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function PlayListViz(spotifyData) {
+
+}
+
+// x would be the data from spotify api call for list of playlists.
+let x;
+
+
+export default function App() {
+    <PlayListViz spotifyData={x}/>
+}
